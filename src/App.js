d29034c20d65d2645636dea41144e0ec465bbbcd
iambunty45 <kasodariyadhruvil45@gmail.com>
2023-06-01 23:27:36 +0530

    login page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Auth from './routes/Auth/auth.routes';
+
+function App() {
+  return (
+    <div className="App">
+   <Auth />
+    </div>
+  );
+}
+
+export default App;
